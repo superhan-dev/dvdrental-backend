1245764679434b195a7d0eb1be816c1e863c58e7
superhan-dev <superhan.dev@gmail.com>
2022-03-04 20:44:42 +0900

    first commit

---
+++ src/main/java/com/superhan/dvdrentalspringbootbackend/DvdrentalSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.superhan.dvdrentalspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalSpringBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvdrentalSpringBootBackendApplication.class, args);
+    }
+
+}
