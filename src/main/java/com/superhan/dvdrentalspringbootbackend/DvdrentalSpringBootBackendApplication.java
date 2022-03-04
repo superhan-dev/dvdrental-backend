1c7eafbdd5f81d3aa8a05c5cd2673a98de685340
superhan-dev <superhan.dev@gmail.com>
2022-03-04 20:46:02 +0900

    first commit

--- src/main/java/com/superhan/dvdrentalspringbootbackend/DvdrentalSpringBootBackendApplication.java
+++ src/main/java/com/superhan/dvdrentalspringbootbackend/DvdrentalSpringBootBackendApplication.java
@@ -8,6 +8,7 @@ public class DvdrentalSpringBootBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DvdrentalSpringBootBackendApplication.class, args);
+
     }
 
 }
