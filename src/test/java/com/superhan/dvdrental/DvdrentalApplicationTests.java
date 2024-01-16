bb83af9c785013984ab547b386a41162771e90fa
superhan-dev <superhan.dev@gmail.com>
2024-01-16 13:04:17 +1100

    spring boot init for a testing gir actions

---
+++ src/test/java/com/superhan/dvdrental/DvdrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superhan.dvdrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
